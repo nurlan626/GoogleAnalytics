9ca579b0c843634c1b16eed5ce31e6d0946b9868
nurlan007 <nurlan@artifactlabs.io>
2022-06-02 21:32:16 +0400

    we

--- src/main.js
+++ src/main.js
@@ -1,4 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import VueGtag from "vue-gtag";
 
-createApp(App).mount('#app')
+// import VueAnalytics from 'vue-analytics';
+// import router from "./router"
+ 
+// const app = createApp(App);
+
+// createApp(App).use(VueAnalytics, {
+//     id: 'G-759FCGFY76',
+    
+// }); 
+createApp(App).use(VueGtag, {
+    config: { id: "G-759FCGFY76" }
+  }).mount("#app");
